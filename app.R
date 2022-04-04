f1a8353e98f78c1f50c4b400db830c2585271770
Pedro Gonzalez <bm.pedro.gonzalez@hotmail.com>
2022-04-04 15:08:07 -0700

    update code

--- app.R
+++ app.R
@@ -9,11 +9,16 @@
 
 library(pacman)
 p_load("shiny", "shinydashboard", "leaflet",
-       "tidyverse", "htmltools", "dplyr")
+       "tidyverse", "htmltools", "dplyr", "shinythemes")
 
 #load dataframe
 nexus_df <- read.csv(file = 'Ocean_Nexus_Projects_list.csv', 
                      sep = ',', skip = 1)
+Text = paste0("<b>Title:</b> ", nexus_df$Title,
+              "<br>", "<b>PI:</b> ", nexus_df$PI.institution,
+              "<br>", "<b>Research or Student Fellow / Research Associate:</b> ", nexus_df$Fellow,       
+              "<br>", "<b>Description:</b> ", nexus_df$Description,
+              "<br>", "<b>Progress:</b> ", nexus_df$Progress)
 
 # Define UI for application that draws a map
 ui <- fluidPage(theme = shinytheme("superhero"),
